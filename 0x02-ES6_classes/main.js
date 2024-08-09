9aeb3ca0b0145199fc41f681a4b76037fc4f23fc
PcNerd9 <ajayihabeeb977@gmail.com>
2024-08-09 21:22:33 +0000

    resolve the bug in the 4-pricing.js file v2

--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -1,13 +1,10 @@
-import Building from "./5-building.js";
+import Pricing from './4-pricing.js';
+import Currency from './3-currency.js';
 
-const b = new Building(100);
-console.log(b);
+const p = new Pricing(100, new Currency("EUR", "Euro"))
+console.log(p);
+p.amount = 300;
+p.currency = new Currency("$", "Dollar");
+console.log(p.displayFullPrice());
+console.log(Pricing.convertPrice(100, 2));
 
-class TestBuilding extends Building {}
-
-try {
-	new TestBuilding(200);
-}
-catch(err) {
-	console.log(err);
-}

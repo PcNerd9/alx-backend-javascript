783bba6de6c52f60a395acb4635862f85dd1fa68
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-08-11 10:17:54 +0100

    resolve the bug int the 6-final-user.js file v2

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,15 +3,9 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
     return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results => {
-        const array = [];
-        results.forEach((result, index) => {
-            array.push({
+    .then((results) => results.map((result) => ({
                 status: result.status,
-                value: result,
-            })
-    
-        });
-        console.log(array);
-    }))
+                value: result.status === 'rejected' ? result.reason.message : result.value,
+            }))
+    )
 }
--- 0x01-ES6_promise/test.js
+++ 0x01-ES6_promise/test.js
@@ -1,9 +1,12 @@
-import handleProfileSignup from "./3-all";
+import handleProfileSignup from './6-final-user';
 
-test("handleProfileSignup returns the right text", async () => {
-    const spy = jest.spyOn(console, "log").mockImplementation();
-  
-    await handleProfileSignup();
-    expect(spy).toBeCalledWith('photo-profile-1 Guillaume Salva');
-    spy.mockRestore();
-  });
+test("handleProfileSignup returns the right array", async () => {
+    const queue = await handleProfileSignup('John', 'Doe', 'Gerald.jpg');
+    expect(queue).toEqual([
+      {
+        status: 'fulfilled',
+        value: { firstName: 'John', lastName: 'Doe' }
+      },
+      { status: 'rejected', value: 'Error: Gerald.jpg cannot be processed' }
+    ]);
+});

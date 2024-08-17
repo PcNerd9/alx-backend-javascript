e58659bd0ed049666f3fe69306c8985f6759fd05
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-08-17 16:52:48 +0100

    resolve the bug in the 8-clean_set.js file v2

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,9 @@
 export default function cleanSet(set, startString) {
-	let concatString = ''
-	
-	if (startString === '') {
-		return concatString;
-	}
-	for (const element of set) {
-		if (element.startsWith(startString)) {
-			concatString += element.slice(startString.length);
-			concatString += '-';
-		}
-	};
-	if (concatString.endsWith("-")) {
-		concatString = concatString.slice(0, -1);
-	}
-	return concatString;
+  if ((set instanceof Set) && (typeof startString === 'string')) {
+    let array = Array.from(set);
+    const len = startString.length;
+    array = array.filter((a) => typeof a === 'string' && a[0] === startString[0] && a.startsWith(startString));
+    return array.map((s) => s.substring(len)).join('-');
+  }
+  return '';
 }

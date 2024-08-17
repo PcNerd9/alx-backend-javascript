419c6876f9debf4afa55be60cca574940d153c5c
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-08-17 16:18:52 +0100

    resolve the bug in the 0-get_list_student.js file v3

--- 0x03-ES6_data_manipulation/test.js
+++ 0x03-ES6_data_manipulation/test.js
@@ -1,9 +1,9 @@
+import getListStudentIds from "./1-get_list_student_ids.js";
 import getListStudents from "./0-get_list_students.js";
 
-test("getListStudents is implemented correctly", () => {
-  expect(getListStudents()).toEqual([
-    { firstName: "Guillaume", id: 1, location: 'San Francisco' },
-    { firstName: "James", id: 2, location: 'Columbia' },
-    { firstName: "Serena", id: 5, location: 'San Francisco' }
-  ]);
+test("getListStudentIds is implemented correctly", () => {
+  expect(getListStudentIds(getListStudents()).length).toBe(3);
+  expect(getListStudentIds(getListStudents())).toEqual([1, 2, 5]);
+  expect(getListStudentIds(null)).toEqual([]);
+  expect(getListStudentIds('TEST')).toEqual([]);
 });
